388ce7d83d3e66d488091e233fbc38cc7f6b496b
EversonSilva99 <everson.cordeiro@aluno.faculdadeimpacta.com.br>
2020-09-24 20:53:44 -0300

    commitando a toolbar que não tinha ido ontem por volta de 00:00

--- SecureDisk2/app/src/main/java/br/com/impacta/techeath/securedisk/MainActivity.kt
+++ SecureDisk2/app/src/main/java/br/com/impacta/techeath/securedisk/MainActivity.kt
@@ -3,17 +3,44 @@ package br.com.impacta.techeath.securedisk
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.toolbar.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        webView.loadUrl("localhost:8000/")
-        btnEmergencia.setOnClickListener {
-            val intent = Intent(this, EmergencyActivity::class.java)
-            startActivity(intent)
+//        setSupportActionBar(toolbar_view)
+        webView.loadUrl("192.168.15.13:8000/")
+    }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar, menu)
+//        return true
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item?.itemId
+
+        if(id == R.id.btnEmergencia)
+            startActivity(Intent(this, EmergencyActivity::class.java))
+            Toast.makeText(this,"Iniciando chamado de emergência", Toast.LENGTH_LONG).show()
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onBackPressed() {
+        // verifica se é possível voltar para
+        // uma pagina anterior no WebView
+        if (webView.canGoBack()) {
+            //volta para pagina anterior
+            webView.goBack()
+        } else {
+            // finaliza atividade
+            finish()
         }
     }
 }
